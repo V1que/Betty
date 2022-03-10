74d0a65ffb3966fb802747c7a4543eed1101334c
V1que <ekpenyongvictoria2@gmail.com>
2022-03-10 20:56:08 +0100

    m

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/*main is the entry point of the project
+Returm: always (0)*/
+int main(void)
+{
+    puts("Programming is like buildinga multilingual puzzle");
+    return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("with proper grammar, but theoutcome is a piece of art,\n");
+    return(0);
+}
